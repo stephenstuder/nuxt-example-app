baa5de6d9cccf393f641a90cf7a3e54b7e62b885
Stephen Studer <stephenkstuder@gmail.com>
2021-01-03 19:03:22 -0700

    meta tags added to pages

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,21 @@
     <h1>Events</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Listing- Real World Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Where you can find all the events taking place in your neighborhood',
+        },
+      ],
+    }
+  },
+}
+</script>
