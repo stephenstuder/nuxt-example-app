5dfdae10dfbc82fd49c4c5f1510e3e6fd2a58e67
Stephen Studer <stephenkstuder@gmail.com>
2021-01-09 20:42:40 -0700

    axios request to json server, component for displaying data

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard.vue'
 export default {
+  components: { EventCard },
+  asyncData({ $axios }) {
+    return $axios.get('http://localhost:3000/events').then((response) => {
+      return {
+        events: response.data,
+      }
+    })
+  },
   head() {
     return {
       title: 'Event Listing',
