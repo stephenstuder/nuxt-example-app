b1f234a5f8de0e0b55051ab6c421b6e445d389f0
Stephen Studer <stephenkstuder@gmail.com>
2021-01-10 19:12:20 -0700

    added in error handling for events call

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,20 @@
 import EventCard from '~/components/EventCard.vue'
 export default {
   components: { EventCard },
-  asyncData({ $axios }) {
-    return $axios.get('http://localhost:3000/events').then((response) => {
-      return {
-        events: response.data,
-      }
-    })
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Opps, We Bonked. Please Try Again.',
+        })
+      })
   },
   head() {
     return {
